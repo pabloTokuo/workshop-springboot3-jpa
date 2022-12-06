dd12a2d498bfdf33ff44d0a4d40237a92774c289
Pablo Tokuo <pbo_1996@hotmail.com>
2022-12-06 10:08:27 -0300

    OrderStatus enum

--- src/main/java/com/springcourse/course/entities/Order.java
+++ src/main/java/com/springcourse/course/entities/Order.java
@@ -1,6 +1,7 @@
 package com.springcourse.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.springcourse.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -19,6 +20,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne // Muitos para um
     @JoinColumn(name = "client_id")
     private User client;
@@ -26,9 +29,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus,User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -46,6 +50,19 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+// Pegando o numero inteiro e transformando em OrderStatus
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+//    Guardar um numero inteiro
+    public void setOrderStatus(OrderStatus orderStatus) {
+//        Metodo defensivo, caso o programador passe um OrderStatus nulo
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
---
+++ src/main/java/com/springcourse/course/entities/enums/OrderStatus.java
@@ -0,0 +1,30 @@
+package com.springcourse.course.entities.enums;
+
+public enum OrderStatus {
+    WAITING_PAYMENT(1),
+    PAID(2),
+    SHIPPED(3),
+    DELIVERED(4),
+    CANCELED(5);
+
+    private final int code;
+
+    private OrderStatus(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    // Metodo statico para verificar qual o status do pedido
+    public static OrderStatus valueOf(int code) {
+        for (OrderStatus value : OrderStatus.values()) {
+            if (value.getCode() == code) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("Invalid OrderStatus code!");
+    }
+
+}
