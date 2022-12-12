f108e945e0826b3f6a66b2bca301720a1cd0e6ce
pabloTokuo <contatopbo@hotmail.com>
2022-12-12 10:33:38 -0300

    Subtotal & Total methods

--- src/main/java/com/springcourse/course/entities/Order.java
+++ src/main/java/com/springcourse/course/entities/Order.java
@@ -97,6 +97,15 @@ public class Order implements Serializable {
         this.payment = payment;
     }
 
+    // Metodo preco total items
+    public Double getTotal(){
+        double sum = 0.0;
+        for (OrderItem x : items) { // Varrer x na lista de items
+            sum += x.getSubTotal();
+        }
+        return sum;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
